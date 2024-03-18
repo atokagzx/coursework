8bb33d0b56c560dea7cfcad68119a3456afb6f7c
Yaroslav Savelev <yar21sav@gmail.com>
2024-03-18 10:44:44 +0300

    update docker scripts

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -6,7 +6,7 @@ cd $ROOT_DIR
 
 docker build -t videobot-img -f $ROOT_DIR/docker/Dockerfile $ROOT_DIR \
                                 --network=host \
-                                --build-arg from=ubuntu:18.04
+                                --build-arg from=ubuntu:20.04
 
 
 cd $EXEC_PATH
